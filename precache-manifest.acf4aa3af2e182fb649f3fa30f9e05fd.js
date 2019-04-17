af6a2fbc8eeb73b50ca8bc98f75fa57a44884736
Tod <kahlewil@uw.edu>
2019-04-17 15:25:15 -0700

    Updates

---
+++ precache-manifest.acf4aa3af2e182fb649f3fa30f9e05fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f2215c24775eb1448e1",
+    "url": "/tkahler/InBrowserFileSystem/static/js/runtime~main.7d614f7d.js"
+  },
+  {
+    "revision": "6872fd29d85f56a44837",
+    "url": "/tkahler/InBrowserFileSystem/static/js/main.24f44f79.chunk.js"
+  },
+  {
+    "revision": "4e5bde1b9ead0c360013",
+    "url": "/tkahler/InBrowserFileSystem/static/js/2.14807521.chunk.js"
+  },
+  {
+    "revision": "6872fd29d85f56a44837",
+    "url": "/tkahler/InBrowserFileSystem/static/css/main.ca894c96.chunk.css"
+  },
+  {
+    "revision": "b8760a6fb2dd4ede7904cd1fe0289bc6",
+    "url": "/tkahler/InBrowserFileSystem/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tkahler/FileSystemBuild/precache-manifest.f5dd5c62694d1a14b0401a343a4becb6.js"
+  "/tkahler/InBrowserFileSystem/precache-manifest.acf4aa3af2e182fb649f3fa30f9e05fd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/tkahler/FileSystemBuild/index.html", {
+workbox.routing.registerNavigationRoute("/tkahler/InBrowserFileSystem/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
